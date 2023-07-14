ed36e39ddfdda99b4ee866d8613485686ef47e59
劉岳宬(Leon Liu) <123029@trone.inc>
2023-07-14 09:23:32 +0800

    公司上傳-0714

--- fileRead.js
+++ fileRead.js
@@ -1,1 +1,17 @@
 const fs = require('fs');       //1. 導入fs 模組
+
+
+//2. 非同步讀取
+// fs.readFile('./readFile.txt', (err, data)=>{
+//     if(err){
+//         console.log('讀取失敗:',err);
+//         return;
+//     }
+//     console.log(data.toString());
+// });
+
+
+//3. 同步讀取
+const data = fs.readFileSync('./readFile.txt')
+
+console.log(data.toString());
